e31f551259b1e7a28744723ff69dd97e6a2d78d0
jakfol1217 <56119515+jakfol1217@users.noreply.github.com>
2021-07-06 20:57:00 +0200

    add variables

--- Game/src/Game.java
+++ Game/src/Game.java
@@ -1,2 +1,11 @@
+import java.util.regex.Pattern;
+
 public class Game {
+    private int turn = -1;
+    private static final String[] turnName = {"WHITE", "", "BLACK"};
+    private static final Pattern fieldPattern = Pattern.compile("[a-hA-H][1-8]");
+    public static int translateField(String position){
+
+        return 0;
+    }
 }
